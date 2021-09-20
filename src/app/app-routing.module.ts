459a5253ee820483e68f3487a88d272e0086d083
Santiago Rodriguez <sato1396@gmail.com>
2021-09-19 23:09:55 -0500

    Arreglo imagen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent}
 ];
 
